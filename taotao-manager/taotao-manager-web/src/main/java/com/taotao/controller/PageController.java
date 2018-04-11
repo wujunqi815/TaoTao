1f2ddf9f7c9f3401d8ed0883089a86950b362806
wujunqi815 <594597399@qq.com>
2018-04-11 16:52:02 +0800

    Initial commit

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,28 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面跳转
+ * @author jason.wu
+ *
+ */
+@Controller
+public class PageController {
+
+	/**
+	 * 首页
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String showIndex(){
+		return "index";
+	}
+	
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page){
+		return page;
+	}
+}
