697c9252d141d358de90300ac825b75e3042bfd0
wujunqi815 <594597399@qq.com>
2018-05-11 13:59:17 +0800

    add project taotao-order
    and add the function createOrder

---
+++ taoto-order/src/main/java/com/taotao/order/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.taotao.order.controller;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.taotao.order.pojo.Order;
+import com.taotao.order.service.OrderService;
+import com.taotao.pojo.TaotaoResult;
+
+@Controller
+public class OrderController {
+
+	@Autowired
+	private OrderService orderService;
+	
+	/**
+	 * 创建订单
+	 */
+	@RequestMapping("/create")
+	public TaotaoResult createOrder(@RequestBody Order order){
+		try{
+			TaotaoResult result = orderService.createOrder(order, order.getOrderItems(), order.getOrderShippings());
+			return result;
+		}catch(Exception e){
+			e.printStackTrace();
+			return TaotaoResult.build(500, ExceptionUtils.getStackTrace(e));
+		}
+	}
+}
---
+++ taoto-order/src/main/java/com/taotao/order/service/impl/OrderServiceImpl.java
@@ -0,0 +1,65 @@
+package com.taotao.order.service.impl;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbOrderItemMapper;
+import com.taotao.mapper.TbOrderMapper;
+import com.taotao.mapper.TbOrderShippingMapper;
+import com.taotao.order.service.OrderService;
+import com.taotao.pojo.TaotaoResult;
+import com.taotao.pojo.TbOrder;
+import com.taotao.pojo.TbOrderItem;
+import com.taotao.pojo.TbOrderShipping;
+
+/**
+ * 订单管理Service
+ * @author jason.wu
+ *
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+
+	@Autowired
+	private TbOrderMapper orderMapper;
+	@Autowired
+	private TbOrderShippingMapper orderShippingMapper;
+	@Autowired
+	private TbOrderItemMapper orderItemMapper;
+	
+	@Override
+	public TaotaoResult createOrder(TbOrder order, List<TbOrderItem> itemList, TbOrderShipping orderShipping) {
+		
+		String uuid = UUID.randomUUID().toString();
+		String orderId = "TAOTAO_" + uuid + order.getBuyerNick();
+		
+		order.setOrderId(orderId);
+		//状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭
+		order.setStatus(1);
+		order.setCreateTime(new Date());
+		order.setUpdateTime(new Date());
+		//买家是否已经评价,0:未评价 1：已评价
+		order.setBuyerRate(0);
+		orderMapper.insert(order);
+		
+		//插入订单明细
+		for(TbOrderItem orderItem : itemList){
+			String newUuid = UUID.randomUUID().toString();
+			String orderDetailId = "TAOTAO_ORDER_ID_" + newUuid + order.getBuyerNick();
+			orderItem.setId(orderDetailId + "");
+			orderItem.setOrderId(orderId + "");
+			orderItemMapper.insert(orderItem);
+		}
+		
+		//插入物流表
+		orderShipping.setOrderId(orderId + "");
+		orderShipping.setCreated(new Date());
+		orderShipping.setUpdated(new Date());
+		return TaotaoResult.ok(orderId);
+	}
+
+}
